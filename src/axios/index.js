d103797d04c325f8bc6b130213ed30ddf32eac6b
justin.lin <justin.lin@yxunistar.com>
2021-04-13 18:00:59 +0800

    axios cache

---
+++ src/axios/index.js
@@ -0,0 +1,23 @@
+import {cacheAdapterEnhancer} from "./utils.js";
+
+
+const instance = axios.create({
+    baseURL: "https://jsonplaceholder.typicode.com",
+    adapter: cacheAdapterEnhancer(axios.defaults.adapter, {
+        enabledByDefault: false,
+        maxAge: 5000,
+    }),
+});
+
+async function requestWithCache() {
+    const response = await instance.get("/todos/1", {cache: true});
+    console.dir(response);
+}
+
+async function requestWithoutCache() {
+    const response = await instance.get("/todos/1", {cache: false});
+    console.dir(response);
+}
+
+document.querySelector("#withCache").addEventListener("click", requestWithCache);
+document.querySelector("#withoutCache").addEventListener("click", requestWithoutCache);
---
+++ src/axios/utils.js
@@ -0,0 +1,79 @@
+const MemoryCache = {
+    data: {},
+    set(key, value, maxAge) {
+        this.data[key] = {
+            maxAge: maxAge || 0,
+            value,
+            now: Date.now(),
+        };
+    },
+    get(key) {
+        const cachedItem = this.data[key];
+        if (!cachedItem) return null;
+        const isExpired = Date.now() - cachedItem.now > cachedItem.maxAge;
+        isExpired && this.delete(key);
+        return isExpired ? null : cachedItem.value;
+    },
+    delete(key) {
+        return delete this.data[key];
+    },
+    clear(){
+        this.data = {};
+    }
+}
+
+export function generateReqKey(config) {
+    const { method, url, params, data } = config;
+    return [method, url, Qs.stringify(params), Qs.stringify(data)].join(
+        "&"
+    );
+}
+
+export function isCacheLike(cache) {
+    return !!(
+        cache.set &&
+        cache.get &&
+        cache.delete &&
+        cache.clear &&
+        typeof cache.get === "function" &&
+        typeof cache.set === "function" &&
+        typeof cache.delete === "function" &&
+        typeof cache.clear === "function"
+    );
+}
+
+export function cacheAdapterEnhancer(adapter, options) {
+    const {
+        maxAge,
+        enabledByDefault = true,
+        cacheFlag = "cache",
+        defaultCache = MemoryCache,
+    } = options;
+    return (config) => {
+        const { url, method, params, forceUpdate } = config;
+        let useCache =
+            config[cacheFlag] !== undefined && config[cacheFlag] !== null
+                ? config[cacheFlag]
+                : enabledByDefault;
+        if (method === "get" && useCache) {
+            const cache = isCacheLike(useCache) ? useCache : defaultCache;
+            let requestKey = generateReqKey(config);
+            let responsePromise = cache.get(requestKey);
+            if (!responsePromise || forceUpdate) {
+                responsePromise = (async () => {
+                    try {
+                        return await adapter(config);
+                    } catch (reason) {
+                        cache.delete(requestKey);
+                        throw reason;
+                    }
+                })();
+                cache.set(requestKey, responsePromise, maxAge);
+                return responsePromise;
+            }
+            return responsePromise;
+        }
+
+        return adapter(config);
+    };
+}
